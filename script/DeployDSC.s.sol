e5773ee2b0262c27626c5ab6be451fe161203d8b
aditya gaargya <adityagaargya@192.168.1.12>
2023-06-22 22:33:15 +0530

    Deploy script complete

--- script/DeployDSC.s.sol
+++ script/DeployDSC.s.sol
@@ -4,12 +4,35 @@ pragma solidity 0.8.19;
 import "forge-std/Script.sol";
 import "../src/DecentralizedStableCoin.sol";
 import "../src/DSCEngine.sol";
+import "./HelperConfig.s.sol";
 
 contract DeployDSC is Script {
+    address[] public tokenAddresses;
+    address[] public priceFeedAddresses;
+
     function run() external returns (DecentralizedStableCoin, DSCEngine) {
-        vm.startBroadcast();
+        HelperConfig config = new HelperConfig();
+        (
+            address wethUsdPriceFeed,
+            address wbtcUsdPriceFeed,
+            address weth,
+            address wbtc,
+            uint256 deployerKey
+        ) = helperConfig.activeNetworkConfig();
+        tokenAddresses = [weth, wbtc];
+        priceFeedAddresses = [wethUsdPriceFeed, wbtcUsdPriceFeed];
+
+        vm.startBroadcast(deployerKey);
         DecentralizedStableCoin dsc = new DecentralizedStableCoin();
+        DSCEngine engine = new DSCEngine(
+            tokenAddresses,
+            priceFeedAddresses,
+            address(dsc)
+        );
+
+        dsc.transferOwnership(address(engine));
 
         vm.stopBroadcast();
+        return (dsc, engine);
     }
 }
